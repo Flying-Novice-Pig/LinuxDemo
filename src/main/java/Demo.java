f251e2c8ef1908ae50577ba0e6a6f966d3d9fc09
zhangxiaohu <zhangxiaohu@sohh.cn>
2019-08-06 15:32:08 +0800

    xiaohu

---
+++ src/main/java/Demo.java
@@ -0,0 +1,13 @@
+import com.xiaohu.MyThread;
+
+public class Demo {
+    public static void main(String[] args){
+//        String args0= args[0];
+//        String args1= args[1];
+//        System.out.print("==============="+args0);
+//        System.out.print("==============="+args1);
+        MyThread md = new MyThread();
+        md.start();
+
+    }
+}
--- src/main/java/com/xiaohu/Demo.java
+++ src/main/java/com/xiaohu/Demo.java
@@ -1,13 +1,15 @@
 package com.xiaohu;
 
 public class Demo {
-    public static void main(String[] args){
-//        String args0= args[0];
-//        String args1= args[1];
-//        System.out.print("==============="+args0);
-//        System.out.print("==============="+args1);
-        MyThread md = new MyThread();
-        md.start();
+	public static void main(String[] args) {
+		String args0 = args[0];
+		String args1 = args[1];
+	    System.out.println("============="+args0);	
+	    System.out.println("============="+args1);	
+	    MyThread md = new MyThread();
+	    md.start();
+	    
 
-    }
+	}
+
 }
